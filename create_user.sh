11619f01dfe08725a7371e9fc2379ee646562fa4
Afeez Oluwashina Adeboye <91129978+Afeez-AA@users.noreply.github.com>
2024-08-09 09:04:21 +0100

    Update create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-USERNAME=$(echo "$PAM_USER")
-
-# Check if user exists and create if not
-if ! id -u "$USERNAME" >/dev/null 2>&1; then
-    sudo useradd -m "$USERNAME"
+# If a username is provided, don't create a new user
+if [ -n "$PAM_USER" ] && [ "$PAM_USER" != "sshd" ]; then
+    exit 0
 fi
+
+# Generate a random username
+USERNAME=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
+
+# Check if the user already exists
+if id "$USERNAME" &>/dev/null; then
+    exit 0
+fi
+
+# Create the user with a home directory and set the shell to /bin/bash
+useradd -m -s /bin/bash $USERNAME
